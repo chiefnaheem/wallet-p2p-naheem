4a6710090abab9a6beb67c39c6817e97131d1995
naheem adedokun <naheemadedokun21@gmail.com>
2022-06-29 16:47:17 +0100

    fixed bugs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,32 @@
+// import { Module } from '@nestjs/common';
+// // import { AppController } from './app.controller';
+// // import { AppService } from './app.service';
+// import { AuthModule } from './auth/auth.module';
+// import { UsersModule } from './users/users.module';
+// import { TransactionModule } from './transaction/transaction.module';
+
+// @Module({
+//   imports: [AuthModule, UsersModule, TransactionModule],
+//   // controllers: [AppController],
+//   // providers: [AppService],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+// import { UserModule } from './user/user.module';
+
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { TransactionModule } from './transaction/transaction.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, TransactionModule],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    // UserModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
